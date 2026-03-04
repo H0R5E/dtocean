28f48589566abf4efcfdab27a580133e70dc3d56
Mathew Topper <damm_horse@yahoo.co.uk>
2026-03-04 17:26:20 +0000

    chore: try setting DISPLAY env var

--- packages/dtocean-app/src/dtocean_app/utils/display.py
+++ packages/dtocean-app/src/dtocean_app/utils/display.py
@@ -15,14 +15,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from PySide6.QtWidgets import QApplication
 
 
 def is_high_dpi(dpi_freshold=100.0) -> bool:
-    qapp = QApplication.instance()
-    if qapp is None:
-        qapp = QApplication()
+    qapp = QApplication.instance() or QApplication()
 
     if not isinstance(qapp, QApplication):
         return False
