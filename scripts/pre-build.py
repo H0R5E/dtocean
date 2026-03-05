7bb899fa1c1a8e497cd5112f27330e10b3139834
Mathew Topper <damm_horse@yahoo.co.uk>
2026-03-05 13:43:33 +0000

    chore: update pre-build to copy included deps
    Included groups are ignored in isolated builds

--- scripts/pre-build.py
+++ scripts/pre-build.py
@@ -19,6 +19,48 @@ def _get_version(data) -> Optional[str]:
                 return poetry["version"]
 
 
+def _copy_included_main(data: dict) -> dict:
+    poetry = data["tool"]["poetry"]
+    if "group" not in poetry:
+        return data
+
+    group = poetry["group"]
+    if "main" not in group:
+        return data
+
+    main_group = group["main"]
+    if "include-groups" not in main_group:
+        return data
+
+    main_group_includes = main_group["include-groups"]
+
+    if not main_group_includes:
+        return data
+
+    new_data = deepcopy(data)
+    new_data_poetry = new_data["tool"]["poetry"]
+
+    if "dependencies" not in new_data_poetry:
+        new_data_poetry["dependencies"] = {}
+
+    new_data_poetry_deps = new_data_poetry["dependencies"]
+
+    for included_group_name in main_group_includes:
+        if included_group_name not in group:
+            continue
+
+        included_group = group[included_group_name]
+        if "dependencies" not in included_group:
+            continue
+
+        included_group_deps = included_group["dependencies"]
+
+        for k, v in included_group_deps.items():
+            new_data_poetry_deps[k] = v
+
+    return new_data
+
+
 def _convert_project_dependencies(data: dict, pyproject: Path) -> dict:
     project = data["project"]
     if "dependencies" not in project:
@@ -84,11 +126,12 @@ def main(root_dir_path: str | Path):
     with open(pyproject, "rb") as f:
         data = tomllib.load(f)
 
-    if "project" in data:
-        new_data = _convert_project_dependencies(data, pyproject)
-
     if "tool" in data and "poetry" in data["tool"]:
-        new_data = _convert_poetry_dependencies(data, pyproject)
+        new_data = _copy_included_main(data)
+        new_data = _convert_poetry_dependencies(new_data, pyproject)
+
+    if "project" in new_data:
+        new_data = _convert_project_dependencies(new_data, pyproject)
 
     with open(pyproject, "wb") as f:
         tomli_w.dump(new_data, f)
