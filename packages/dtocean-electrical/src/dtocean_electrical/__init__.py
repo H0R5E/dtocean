384623dba54b98ae7dc8be485d32464296eee992
Mathew Topper <damm_horse@yahoo.co.uk>
2026-06-22 12:04:11 +0100

    Update copyright notices

--- packages/dtocean-electrical/src/dtocean_electrical/__init__.py
+++ packages/dtocean-electrical/src/dtocean_electrical/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #    Copyright (C) 2016 Adam Collin, Mathew Topper
-#    Copyright (C) 2019 Mathew Topper
+#    Copyright (C) 2017-2026 Mathew Topper
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
--- packages/dtocean-electrical/src/dtocean_electrical/optim_codes/umbilical.py
+++ packages/dtocean-electrical/src/dtocean_electrical/optim_codes/umbilical.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 #    Copyright (C) 2016 Sam Weller
-#    Copyright (C) 2017-2018 Mathew Topper
+#    Copyright (C) 2016 Adam Collin
+#    Copyright (C) 2017-2026 Mathew Topper
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
